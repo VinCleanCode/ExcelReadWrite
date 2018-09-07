a4e8bab0f04dd043a96084ba5e444951ab2dfed5
vl97 <Vincent.M.Lee@newegg.com>
2018-09-07 17:34:32 +0800

    updated

--- src/main/java/salaryCal/readExcel.java
+++ src/main/java/salaryCal/readExcel.java
@@ -6,15 +6,11 @@ import java.io.IOException;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
-import java.util.Iterator;
 
 public class readExcel {
 
     public static void main(String[] args) throws InvalidFormatException {
         //Creating a Workbook from an Excel file (.xls or .xlsx)
-//        String filePath = "src/main/resources/001_2018_6.xls";
-//        String SAMPLE_XLSX_FILE_PATH = "src/main/resources/001_2018_6.xls";
-
         String SAMPLE_XLSX_FILE_PATH = "src/main/resources/201806.xlsx";
         Workbook workbook;
 
@@ -22,13 +18,6 @@ public class readExcel {
             workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
             System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");    
 
-            // 1. You can obtain a sheetIterator and iterate over it
-            Iterator<Sheet> sheetIterator = workbook.sheetIterator();
-            System.out.println("Retrieving Sheets using Iterator");
-            while (sheetIterator.hasNext()) {
-                Sheet sheet = sheetIterator.next();
-                System.out.println("=> " + sheet.getSheetName());
-            }
             // 2. Or you can use a for-each loop
             System.out.println("Retrieving Sheets using for-each loop");
             for(Sheet sheet: workbook) {
