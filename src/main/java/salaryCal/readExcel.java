484983f7a67c8811bd89c3a3de61cf00cec49bd2
vl97 <Vincent.M.Lee@newegg.com>
2018-09-07 17:13:41 +0800

    read file from excel. 讀取格式不符合問題待解決

--- src/main/java/salaryCal/readExcel.java
+++ src/main/java/salaryCal/readExcel.java
@@ -1,8 +1,30 @@
 package salaryCal;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+
 public class readExcel {
 
 	public static void main(String[] args) {
-		System.out.println("Ya Ya Ya");
+        //Creating a Workbook from an Excel file (.xls or .xlsx)
+	    String filePath = "src/main/resources/001_2018_6.xls";
+	    Workbook workbook;
+        
+	    try {
+            workbook = WorkbookFactory.create(new File(filePath));
+            System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");    
+            
+        } catch (EncryptedDocumentException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
 	}
 }
